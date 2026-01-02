63989204a1008e24a1d09930d8fd0b3c581d5216
Maley Venkatesh <venkateshmaley1@gmail.com>
2026-01-02 09:48:02 +0530

    changes

--- 3_Varibles/InstanceVar.java
+++ 3_Varibles/InstanceVar.java
@@ -1,4 +1,8 @@
+//Exaplanation: 
 //"Instance Varibles" are non-static Variables and are declared in a Class outside of any Method,Constuctor or Block.
+
+//Problem: 
+//Write a example program to explain Instance Variable
 
 import java.io.*;
 
@@ -15,3 +19,5 @@ class InstanceVar{
         System.out.println("Geek name is:" + name.geek);
     }
 }
+
+
